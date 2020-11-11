6e6a112a769e628b2063722baf628e98bbeda42c
INTLSavio <savio.leite@gec.inatel.br>
2020-11-11 00:44:09 -0300

    Desafio6

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Transaction value is greater than available total');
+    }
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      const categoryCreated = categoriesRepository.create({ title: category });
+
+      await categoriesRepository.save(categoryCreated);
+    }
+
+    const currentCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: currentCategory?.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
